d249ee847791196eb05003d5e9b97143f7b98d83
Sam Edwards-Marsh <sam.david.marsh@gmail.com>
2024-07-02 23:43:49 -0700

    added getMeanBpm()

--- src/taps.ts
+++ src/taps.ts
@@ -6,7 +6,7 @@
 class Taps {
   private static instance: Taps;
   private tapTimes_ms: number[] = [];
-  private maxTaps: number = 3;
+  private maxTaps: number = 20;
 
   /**
    * Creates an instance of Taps.
@@ -46,9 +46,31 @@ class Taps {
     this.tapTimes_ms.push(time_ms);
   }
 
-  // public static getMeanBpm(): number {
+  /**
+   * Get the mean (average) BPM.
+   *
+   * @return {*}  {number} Average.
+   * @memberof Taps
+   */
+  public getMeanBpm(): number {
+    // Calculate average tap period (time between taps).
+    const tapPeriods_ms = [];
+    for (let i = 1; i < this.tapTimes_ms.length; i++) {
+      tapPeriods_ms.push(this.tapTimes_ms[i] - this.tapTimes_ms[i - 1]);
+    }
+    const avgTapPeriod_ms =
+      tapPeriods_ms.reduce((acc: number, curr: number) => acc + curr, 0) /
+      this.tapTimes_ms.length;
+    console.log("avgTapPeriod", avgTapPeriod_ms);
 
-  // }
+    // Calculate average bpm.
+    const oneMinuet_ms = 60 * 1000;
+    const avgBpm = Math.round(oneMinuet_ms / avgTapPeriod_ms);
+    console.log("avgBpm: ", avgBpm);
+    console.log("taps: ", this.tapTimes_ms);
+
+    return avgBpm
+  }
 
   // public static getMedianBpm(): number
   // public static getModeBpm(): number
