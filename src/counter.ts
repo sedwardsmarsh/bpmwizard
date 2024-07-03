1d41056456a61617f89d97c735230d7f800df5af
Sam Edwards-Marsh <sam.david.marsh@gmail.com>
2024-07-02 20:13:30 -0700

    added docstring and comments to setupTempoTap()

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,23 @@
+/**
+ * Setup the tempo tap button.
+ *
+ * @export
+ * @param {HTMLButtonElement} element - Button.
+ */
 export function setupTempoTap(element: HTMLButtonElement) {
-  let counter = 0;
-  const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
+  
+  // Initialize the tap count.
+  let tapCount = 0;
+
+  // Setter for `tapCount`.
+  const setTapCount = (count: number) => {
+    tapCount = count;
+    element.innerHTML = `Tap for tempo ${tapCount}`;
   };
-  element.addEventListener("click", () => setCounter(counter + 1));
-  setCounter(0);
+
+  // Increment `tapCount` when tap button is pressed.
+  element.addEventListener("click", () => setTapCount(tapCount + 1));
+
+  // Initialize the tap count to zero.
+  setTapCount(0);
 }
