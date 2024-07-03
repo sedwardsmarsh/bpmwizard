e5a16e41ca4c4ed205794651c41ded0d501d1dbe
Sam Edwards-Marsh <sam.david.marsh@gmail.com>
2024-07-02 20:19:08 -0700

    added setTapCount()

--- src/counter.ts
+++ src/counter.ts
@@ -4,20 +4,23 @@
  * @export
  * @param {HTMLButtonElement} element - Button.
  */
-export function setupTempoTap(element: HTMLButtonElement) {
-  
+export function setupTempoTap(element: HTMLButtonElement): void {
   // Initialize the tap count.
   let tapCount = 0;
 
-  // Setter for `tapCount`.
-  const setTapCount = (count: number) => {
-    tapCount = count;
-    element.innerHTML = `Tap for tempo ${tapCount}`;
-  };
-
   // Increment `tapCount` when tap button is pressed.
-  element.addEventListener("click", () => setTapCount(tapCount + 1));
+  element.addEventListener("click", () => setTapCount(element, tapCount + 1));
 
   // Initialize the tap count to zero.
-  setTapCount(0);
+  setTapCount(element, 0);
+}
+
+/**
+ * Update the tap count.
+ *
+ * @param {HTMLButtonElement} element
+ * @param {number} tapCount
+ */
+function setTapCount(element: HTMLButtonElement, tapCount: number): void {
+  element.innerHTML = `Tap for tempo ${tapCount}`;
 }
