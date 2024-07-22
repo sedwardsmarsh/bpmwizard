76922ecb8295dd47b9537f1e081f9c25da8c314c
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2024-07-21 23:29:50 -0700

    converted all bpm wizard ids to classes

--- src/main.ts
+++ src/main.ts
@@ -8,39 +8,39 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       BPM Wizard
     </h1>
 
-    <div id="readout">
+    <div class="readout">
 
       <div class="statRow">
         <p class="desc">meanBpm</p> 
-        <p class="data" id="meanBpm">0</p>
+        <p class="data meanBpm">0</p>
       </div>
 
       <div class="statRow">
         <p class="desc">medianBpm</p> 
-        <p class="data" id="medianBpm">0</p>
+        <p class="data medianBpm">0</p>
       </div>
 
       <div class="statRow">
         <p class="desc">modeBpm</p> 
-        <p class="data" id="modeBpm">0</p>
+        <p class="data modeBpm">0</p>
       </div>
 
       <div class="statRow">
         <p class="desc">stddBpm</p> 
-        <p class="data" id="stddBpm">0</p>
+        <p class="data stddBpm">0</p>
       </div>
       
     </div>
 
-    <button id="tap"/>Tap Me</button>
+    <button class="tap"/>Tap Me</button>
   
   </div>
 `;
 
 setupTempoTap(
-  document.querySelector<HTMLButtonElement>("#tap")!,
-  document.querySelector<HTMLParagraphElement>("#meanBpm")!,
-  document.querySelector<HTMLParagraphElement>("#medianBpm")!,
-  document.querySelector<HTMLParagraphElement>("#modeBpm")!,
-  document.querySelector<HTMLParagraphElement>("#stddBpm")!
+  document.querySelector<HTMLButtonElement>(".tap")!,
+  document.querySelector<HTMLParagraphElement>(".meanBpm")!,
+  document.querySelector<HTMLParagraphElement>(".medianBpm")!,
+  document.querySelector<HTMLParagraphElement>(".modeBpm")!,
+  document.querySelector<HTMLParagraphElement>(".stddBpm")!
 );
